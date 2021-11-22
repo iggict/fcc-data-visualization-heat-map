bffca779bc46bef4fa1aedb5936dbe57d048a9d0
iggict <58374304+iggict@users.noreply.github.com>
2021-11-22 11:14:54 +0100

    Update script.js
    Project submitted

--- dist/script.js
+++ dist/script.js
@@ -3,8 +3,8 @@
 const margin = {
   top: 100,
   right: 20,
-  bottom: 70,
-  left: 80,
+  bottom: 100,
+  left: 95,
 };
 
 const parentWidth = 1000;
@@ -23,6 +23,8 @@ const yScale = d3.scaleBand().range([0, height]);
 
 /** D3 containers */
 
+// Map
+
 const graph = d3
   .select("body")
   .append("div")
@@ -34,6 +36,8 @@ const graph = d3
   .attr("height", parentHeight)
   .append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Axis
 
 const xAxis = graph
   .append("g")
@@ -47,6 +51,25 @@ const yAxis = graph
   .attr("id", "y-axis")
   .attr("class", "axis y-axis");
   // call will set in data loading
+
+// Axis labels
+
+const xLabel = xAxis
+  .append("text")
+  .attr("class", "label")
+  .attr("x", width)
+  .attr("y", 50)
+  .text("Years");
+
+const yLabel = yAxis
+  .append("text")
+  .attr("class", "label")
+  .attr("transform", "rotate(-90)")
+  .attr("x", 0)
+  .attr("y", -65)
+  .text("Months");
+
+// Headers
 
 const title = graph
   .append("text")
@@ -64,29 +87,24 @@ const subtitle = graph
   .attr("y", -margin.top / 2 + 30)
   // Text will set in data loading
 
+// Legend
+
+ const legendTop = height + 50
+
+ const legend = graph
+    .append("g")
+    .attr("id", "legend")
+    .attr("class", "legend")
+    .attr("transform",`translate(0,${legendTop})`); 
+
+// Tooltip
+
 const tooltip = d3
   .select("body")
   .append("div")
   .attr("id", "tooltip")
   .attr("class", "tooltip")
 ;
-
-/** Axis labels */
-
-const xLabel = xAxis
-  .append("text")
-  .attr("class", "label")
-  .attr("x", width)
-  .attr("y", 50)
-  .text("Years");
-
-const yLabel = yAxis
-  .append("text")
-  .attr("class", "label")
-  .attr("transform", "rotate(-90)")
-  .attr("x", 0)
-  .attr("y", -45)
-  .text("Months");
 
 /** Load data */
 
@@ -101,7 +119,8 @@ d3.json(JSONFile)
     const baseTemp = data.baseTemperature;
  
     const dataset = data.monthlyVariance.map(d => (
-      {...d, temperature: parseFloat((baseTemp + d.variance).toFixed(3))}
+      //{...d, temperature: parseFloat((baseTemp + d.variance).toFixed(3))}
+      {...d, temperature: baseTemp + d.variance}
     ));
   
     const [minYear, maxYear] = d3.extent(dataset, (d) => d.year);
@@ -113,12 +132,15 @@ d3.json(JSONFile)
       .text(`Base temperature: ${baseTemp}℃ (${minYear} to ${maxYear})`);
   
     /** Color Brewer */
-        
-    const colors = d3
+    
+    const numOfColors = 10;
+  
+    const colorRange = d3.schemeRdYlBu[numOfColors].reverse();
+  
+    const colorScale = d3
       .scaleQuantize()
       .domain([minTemp, maxTemp]) 
-      .range(d3.schemeRdYlBu[10].reverse()) // 10 = num of colors
-     ;
+      .range(colorRange);
  
    /** Set scale domain */
   
@@ -128,7 +150,7 @@ d3.json(JSONFile)
     /** Set axis */
 
     const monthNumToName = (monthNum) => {
-      const formatMonth = d3.timeFormat('%b');
+      const formatMonth = d3.timeFormat('%B');
       // Note: setUTCMonth expects values from 0 to 11
       return formatMonth(new Date().setUTCMonth(monthNum-1)); 
     };
@@ -154,15 +176,22 @@ d3.json(JSONFile)
       .enter()
       .append("rect")
       .attr("class", "cell")
+      .attr("data-year", d => d.year)
+      /* 
+        data-month:
+        Range to pass the test is 0 - 11.
+        Range of month in JSON File is 1 - 12.
+        ¿Is this a bug? 
+      */
+      .attr("data-month", d => d.month - 1) // -1 to pass user story #8
+      .attr("data-temp", d => d.temperature)
       .attr("x", (d) => xScale(d.year))
       .attr("y", (d) => yScale(d.month))
       .attr("width", (d) => xScale.bandwidth(d.year))
       .attr("height", (d) => yScale.bandwidth(d.month))
-      .style("fill", (d) => colors(d.temperature))
+      .style("fill", (d) => colorScale(d.temperature))
       .on("mouseover", (event, d) => {
-        tooltip.transition().duration(300).style("opacity", 0.75);
-        
-        const tooltipMargin = 10;
+        tooltip.transition().duration(300).style("opacity", 0.8);
         
         const tooltipInnerHtml = (item) => (
           `<span class="tt-year">${item.year}</span> - 
@@ -172,17 +201,70 @@ d3.json(JSONFile)
            <br />
            <span class="tt-variance">${item.variance.toFixed(1)}ºC</span>
             <hr class="tt-color" 
-              style="border-color: ${colors(d.temperature)}"/>`
+              style="border-color: ${colorScale(d.temperature)}"/>`
         );
+
+        const [xTooltipMargin, yTooltipMargin] = [20, -40];
         
         tooltip
-          .style("top", (event.pageY || event.x) + tooltipMargin + "px")
-          .style("left", (event.pageX || event.y) + tooltipMargin + "px")
-          .attr("transform", `translate(10, 10`)
+          .style("top", (event.pageY || event.y) + yTooltipMargin + "px")
+          .style("left", (event.pageX || event.x) + xTooltipMargin + "px")
+          .attr("data-year", d.year)
           .html(tooltipInnerHtml(d));
       })
       .on("mouseout", () => {
         tooltip.transition().duration(300).style("opacity", 0);
       });
+  
+      /** Set Legend */
+      
+      const legendItemWidth = 20;
+      const legendWidth = colorRange.length * legendItemWidth;
+      
+      legend
+        .selectAll("rect")
+        .data(colorRange)
+        .enter()
+        .append("rect")
+        .classed("legend-item", true)
+        .style("fill", d => d)
+        .attr("x", (d, i) => (legendItemWidth * i))
+        .attr("y", 0)
+        .attr("width", legendItemWidth)
+        .attr("height", legendItemWidth);
+        
+      // Legend scale
+
+      const legendXScale = d3
+        .scaleLinear()
+        .domain([minTemp, maxTemp])
+        .range([0, legendWidth]);
+
+      // Legend axis
+  
+      const createDomain = (min, max, length) => {
+        const base = min;
+        const step = (max - min) / length;
+        const array = [base];
+        for (let i = 0; i <= length; i++) {
+          array.push (base + i * step);
+        }
+        return array;
+      };
+      
+      const legendXDomain = createDomain(minTemp, maxTemp, colorRange.length);
+  
+      const legendXAxis = graph
+        .append("g")
+        .attr("id", "legend-x-axis")
+        .attr("class", "axis legend-x-axis")
+        .attr("transform", `translate(0, ${legendTop + legendItemWidth})`);
+      
+      legendXAxis.call(d3
+        .axisBottom(legendXScale)
+        .tickValues(legendXDomain)
+        .tickFormat(d3.format('.1f'))
+      );
+
   })
   .catch((err) => console.error(err));  
